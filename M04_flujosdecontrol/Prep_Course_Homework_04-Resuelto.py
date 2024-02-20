1bd460f20366578c760785001349295fdc7b3822
kakodario777 <159160564+kakodario777@users.noreply.github.com>
2024-02-20 01:33:34 -0300

    Update Prep_Course_Homework_04-Resuelto.py

--- M04_flujosdecontrol/Prep_Course_Homework_04-Resuelto.py
+++ M04_flujosdecontrol/Prep_Course_Homework_04-Resuelto.py
@@ -8,13 +8,13 @@
 # In[4]:
 
 
-a = 10
-if (a < 0):
-    print('La variable es menor a cero')
-elif (a > 0): 
-    print('La varaible es mayor a cero')
+a = 5
+if a>0:
+    print("A es mayor que 0")
+elif a<0:
+    print("A es menor que cero")
 else:
-    print('La variable es igual a cero')
+    print("A es igual a 0")
 
 
 # 2) Crear dos variables y un condicional que informe si son del mismo tipo de dato
@@ -22,12 +22,12 @@ else:
 # In[5]:
 
 
-a = 2
-b = 'hola'
-if (type(a) == type(b)):
-    print('Las variables son del mismo tipo de dato')
+a = 5
+b = 25
+if type(a) == type(b):
+    print("A y B Son el mismo tipo de dato")
 else:
-    print('Las variables son de tipos de dato diferentes')
+    print("A y B No son el mismo tipo de dato")
 
 
 # 3) Para los valores enteros del 1 al 20, imprimir por pantalla si es par o impar
@@ -35,11 +35,12 @@ else:
 # In[7]:
 
 
-for i in range(1, 21):
-    if i % 2 == 0:
-        print('El número ', str(i), ' es par')
+for e in range(1,21):
+    if e % 2 == 0:
+        print(str(e),"Es par")
+        
     else:
-        print('El número ', str(i), ' es impar')
+        print(str(e),"No es par")
 
 
 # 4) En un ciclo for mostrar para los valores entre 0 y 5 el resultado de elevarlo a la potencia igual a 3
@@ -47,19 +48,17 @@ for i in range(1, 21):
 # In[9]:
 
 
-for i in range(0, 6):
-    print('Valor:', str(i), ' Elevado a la 3ra potencia:', str(i**3))
+for i in range(0,6):
+    print(str(i), "elevado a la tercera es igual a:", str(i**3))
 
 
 # 5) Crear una variable que contenga un número entero y realizar un ciclo for la misma cantidad de ciclos
 
 # In[10]:
 
-
-n = 12
-for i in range(0, n):
-    pass
-print(i)
+va = 5
+for n in range(0,va):
+     print(n)
 
 
 # 6) Utilizar un ciclo while para realizar el factorial de un número guardado en una variable, sólo si la variable contiene un número entero mayor a 0
@@ -67,18 +66,16 @@ print(i)
 # In[33]:
 
 
-n = 5
-if (type(n) == int):
-    if (n > 0):
-        factorial = n
-        while (n > 2):
-            n = n - 1
-            factorial = factorial * n
-        print('El factorial es', factorial)
-    else:
-        print('La variable no es mayor a cero')
+n=35
+if (type(n)==int):
+  if (n>0):
+    factorial= n
+    while(n>1):
+        n=n-1
+        factorial=factorial*n
+    print("El factorial es igual a:", factorial)
 else:
-    print('La variable no es un entero')
+   print("Este numero no es entero y no es mayor que 0")
     
 
 
@@ -87,25 +84,24 @@ else:
 # In[38]:
 
 
-n = 0
-while(n < 5):
-    n += 1
-    for i in range(1,n):
-        print('Ciclo while nro ' + str(n))
-        print('Ciclo for nro ' + str(i))
-
+num=1
+while num < 6:
+    print("Num con while ", num)
+    num =num+1
+    for i in range(6,11):
+        print("Num con for",i)
+ 
 
 # 8) Crear un ciclo while dentro de un ciclo for
 
 # In[3]:
 
-
-n = 5
-for i in range(1, n):
-    while(n < 5):
-        n -= 1
-        print('Ciclo while nro ' + str(n))
-        print('Ciclo for nro ' + str(i))
+for i in range (1,6):
+    print("Impreso con for:",i)
+    b=6
+    while b<11:
+        print("Impreso con while:",b)
+        b=b+1
 
 
 # 9) Imprimir los números primos existentes entre 0 y 30
@@ -113,18 +109,19 @@ for i in range(1, n):
 # In[54]:
 
 
-tope_rango=30
-n = 0
-primo = True
-while (n < tope_rango):
-    for div in range(2, n):
-        if (n % div == 0):
-            primo = False
-    if (primo):
-        print(n)
-    else:
-        primo = True
-    n += 1
+for num in range(0,31):
+    if num>1:
+        cont=0    
+        i=2
+        while i<num and cont==0:
+                
+            resto=num%i
+            #print("{}%{}={}".format(num,i,resto))
+            if resto==0:
+               cont+=1             
+            i+=1
+        if cont==0:
+            print("El:",num,"es primo")
 
 
 # 10) ¿Se puede mejorar el proceso del punto 9? Utilizar las sentencias break y/ó continue para tal fin
@@ -132,18 +129,7 @@ while (n < tope_rango):
 # In[55]:
 
 
-n = 0
-primo = True
-while (n < tope_rango):
-    for div in range(2, n):
-        if (n % div == 0):
-            primo = False
-            break
-    if (primo):
-        print(n)
-    else:
-        primo = True
-    n += 1
+
 
 
 # 11) En los puntos 9 y 10, se diseño un código que encuentra números primos y además se lo optimizó. ¿Es posible saber en qué medida se optimizó?
@@ -151,41 +137,7 @@ while (n < tope_rango):
 # In[56]:
 
 
-ciclos_sin_break = 0
-n = 0
-primo = True
-while (n < tope_rango):
-    for div in range(2, n):
-        ciclos_sin_break += 1
-        if (n % div == 0):
-            primo = False
-    if (primo):
-        print(n)
-    else:
-        primo = True
-    n += 1
-print('Cantidad de ciclos: ' + str(ciclos_sin_break))
 
-
-# In[57]:
-
-
-ciclos_con_break = 0
-n = 0
-primo = True
-while (n < tope_rango):
-    for div in range(2, n):
-        ciclos_con_break += 1
-        if (n % div == 0):
-            primo = False
-            break
-    if (primo):
-        print(n)
-    else:
-        primo = True
-    n += 1
-print('Cantidad de ciclos: ' + str(ciclos_con_break))
-print('Se optimizó a un ' + str(ciclos_con_break/ciclos_sin_break) + '% de ciclos aplicando break')
 
 
 # 12) Aplicando continue, armar un ciclo while que solo imprima los valores divisibles por 12, dentro del rango de números de 100 a 300
@@ -193,47 +145,46 @@ print('Se optimizó a un ' + str(ciclos_con_break/ciclos_sin_break) + '% de cicl
 # In[62]:
 
 
-n = 99
-while(n <= 300):
-    n += 1
-    if (n % 12 != 0):
-        continue
-    print(n, ' es divisible por 12')
+e=100
 
+while e<300:
+   e+=1
+   if e%12!=0:
+      continue
+   print(e,"es divisible por 12")
 
 # 13) Utilizar la función **input()** que permite hacer ingresos por teclado, para encontrar números primos y dar la opción al usario de buscar el siguiente
 
 # In[73]:
 
 
-n = 1
-sigue = 1
-primo = True
-while (sigue == 1):
-    for div in range(2, n):
-        if (n % div == 0):
-            primo = False
+num=int(input("Ingrese su número:"))
+cont=0
+if num>1:
+    for i in range(2,num):
+        resto=num%i
+        #print("{}%{}={}".format(num,i,resto))
+        if resto==0:
+            cont+=1
             break
-    if (primo):
-        print(n)
-        print('¿Desea encontrar el siguiente número primo?')
-        if (input() != '1'):
-            print('Se finaliza el proceso')
-            break
+    if cont==0:
+        print("El:",num,"es primo")
     else:
-        primo = True
-    n += 1
+        print("El:",num,"no es primo")
+else:
+    print(num,"no es primo")
 
 
 # 14) Crear un ciclo while que encuentre dentro del rango de 100 a 300 el primer número divisible por 3 y además múltiplo de 6
 
 # In[75]:
 
-
-n = 100
-while(n<=300):
-    if (n % 6 == 0):
-        print('El número es: ', str(n))
+tope=300
+e=100
+while (e < tope):
+    if e % 3 == 0 and e % 6==0:
+        print("Es divisible x 3 y es multiplo de 6 el numero:",e)
         break
-    n += 1
+    e+=1
+
 
